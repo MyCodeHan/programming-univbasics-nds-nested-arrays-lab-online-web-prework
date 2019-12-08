c3c2f2f06e526751d04cb538ea7ecf1040bdfc43
Hannah Harrington <hannahharrington@outlook.com>
2019-12-08 20:30:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,6 +39,6 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updatd matrix
-  ENNET_HOUSE[0,5] = new_value
+  ENNET_HOUSE[0,5]
   
   end
